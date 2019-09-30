bb56fe425aab062d924668a6cb116a388b8c4e09
anupriyasaxena <aspriyamail@gmail.com>
2019-09-30 12:39:17 +0530

    Landing page UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (     
+    <Router>
+        <div className="container">
+          <div className="logo-container"><span className="logo"></span></div>
+          <div className="continents-container">
+            <div className="continent">
+              <div className="continent-img continent-img-africa"></div>
+              <div className="continent-text">Africa</div>
+            </div>
+            <div className="continent">
+              <div className="continent-img continent-img-asia"></div>
+              <div className="continent-text">Asia</div>
+            </div>
+            <div className="continent">
+              <div className="continent-img continent-img-europe"></div>
+              <div className="continent-text">Europe</div>
+            </div>
+            <div className="continent">
+              <div className="continent-img continent-img-northAmerica"></div>
+              <div className="continent-text">North America</div>
+            </div>
+            <div className="continent">
+              <div className="continent-img continent-img-australia"></div>
+              <div className="continent-text">Australia</div>
+            </div>
+            <div className="continent">
+              <div className="continent-img continent-img-southAmerica"></div>
+              <div className="continent-text">South America</div>
+            </div>
+          </div>
+          <div className="worldMap-container">
+            <div className="worldMap"></div>
+          </div>
+        </div>
+    </Router>
   );
 }
 
